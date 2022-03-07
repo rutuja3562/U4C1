f2c627d9810dce5082d0776a8f9c01436836620c
rutuja3562 <rspatil3562@gmail.com>
2022-03-07 12:23:44 +0530

    second commit

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const express = require("express");
 const app = express();
 
 app.get("/books",logger,function(req,res){
+    console.log("route: /books")
 return res.send({route: "/books"})
 })
 
 app.get("/libraries",logger,function(req,res){
+    console.log("route: /libraries")
     return res.send({route: "/libraries", permission: true})
 })
 app.get("/authors",logger,function(req,res){
+    console.log("route: /authors")
     return res.send({route: "/authors", permission: true})
 })
 
 function logger(req,res,next){
+    console.log("before logger")
     next();
+    console.log("after logger")
 }
 
 function checkPermission(data){
