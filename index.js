0dedcacb7a40650106f80bbde856c0ff86ee4c96
rutuja3562 <rspatil3562@gmail.com>
2022-03-07 12:16:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/books",logger,function(req,res){
+return res.send({route: "/books"})
+})
+
+app.get("/libraries",logger,function(req,res){
+    return res.send({route: "/libraries", permission: true})
+})
+app.get("/authors",logger,function(req,res){
+    return res.send({route: "/authors", permission: true})
+})
+
+function logger(req,res,next){
+    next();
+}
+
+function checkPermission(data){
+    return function logger1(res,req,next){
+
+       if(data=="librarian"){
+         next();
+       }
+       else if(data = "author"){
+           next();
+       }
+    }
+ 
+
+}
+
+app.listen(5000,()=>{
+    console.log("listen to port 5000")
+})
